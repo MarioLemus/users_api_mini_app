640dcc13b4fd0eeb172b79323125cd7d3af054b2
Mario Lemus <theliarnorland@gmail.com>
2021-07-18 18:07:18 -0600

    fixxing bugs

--- src/Home.js
+++ src/Home.js
@@ -23,32 +23,30 @@ function App() {
           <div className="container">
                 <p className="title">User</p>
                 <div>
-                        {
-                                info.map(data => {
-                                                return (
-                                                        <div className="text-holder" key={data.id}>
-                                                                <div className="user-holder">
-                                                                <p className="text-name id">{ data.name }</p>
-                                                                        <small>
-                                                                                <p className="text">
-                                                                                        <span className="id">Email:</span> { data.email }
-                                                                                </p>
-                                                                        </small>
-
-                                                                        <small>
-                                                                                <p className="text">
-                                                                                        <span className="id">City:</span> { data.address.city }
-                                                                                </p> 
-                                                                        </small>
-                                                                </div>
-
-                                                                <div className="button-holder">
-                                                                        <Link to={ `/user/${data.id}` } className="button">Más info</Link>
-                                                                </div>
-                                                        </div>
-                                                )
-                                })
-                        }
+                    {
+                        info.map(data => {
+                            return (
+                                <div className="text-holder" key={data.id}>
+                                    <div className="user-holder">
+                                    <p className="text-name id">{ data.name }</p>
+                                        <small>
+                                            <p className="text">
+                                                <span className="id">Email:</span> { data.email }
+                                            </p>
+                                        </small>
+                                        <small>
+                                            <p className="text">
+                                                <span className="id">City:</span> { data.address.city }
+                                            </p> 
+                                        </small>
+                                    </div>
+                                    <div className="button-holder">
+                                            <Link to={ `/user/${data.id}` } className="button">Más info</Link>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
           </div>
         );
